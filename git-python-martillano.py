e190097bf2a454e68a4440439e7bc107771b7127
MartillanoBillFrancis <francisfrancisbill@gmail.com>
2022-03-12 11:09:22 +0800

    My Personal Info added

---
+++ git-python-martillano.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 11:02:13 2022
+
+@author: Acer
+"""
+
+print("Bill Francis L. Martillano")
+print("Lupac, Boac, Marinduque")
+print("October 10, 2000")
