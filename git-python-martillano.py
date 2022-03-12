a7a4fcb655c1be1ce25dd9559c00a0c09737d735
MartillanoBillFrancis <francisfrancisbill@gmail.com>
2022-03-12 11:30:34 +0800

    My Educational Background added

--- git-python-martillano.py
+++ git-python-martillano.py
@@ -8,3 +8,6 @@ Created on Sat Mar 12 11:02:13 2022
 print("Bill Francis L. Martillano")
 print("Lupac, Boac, Marinduque")
 print("October 10, 2000")
+print("Marinduque State College")
+print("BS Information Technology")
+print("2nd Year - Section: D")
